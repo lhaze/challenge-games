2752204e683ab765e3f127e7f80906d17271d378
Lukasz Haze <lhaze@lhaze.name>
2017-04-04 23:47:07 +0200

    ResourceStack (WIP)

--- src/core/models/ResourceStack.js
+++ src/core/models/ResourceStack.js
@@ -8,10 +8,15 @@ class ResourceException extends CoreException {}
 
 class ResourceStack {
     constructor(resources) {
-        // throw new ResourceException();
-        if (!_.isEqual(Object.keys(resources), ResourceStack.getResourceLiterals())) {}
-        for (let key in resources) {
-            this[key] = resources[key];
+        throw new ResourceException();
+        if (!_.isEqual(
+            _.keys(resources),
+            ResourceStack.getResourceLiterals()
+        )) {
+            throw new ResourceException();
+        }
+        for (const resource in resources) {
+            this[resource] = resources[resource];
         }
     }
 
@@ -19,8 +24,48 @@ class ResourceStack {
         return new Set();
     }
 
+    isContaining(other_stack) {
+        return _.isEqual(_.keys(this), _.keys(other_stack));
+    }
+
+    equals(other_stack) {
+        for (const resource in other_stack) {
+            if (!this.hasOwnProperty(resource) ||
+                this[resource] !== other_stack[resource]
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    add(added_stack) {
+        for (const resource in added_stack) {
+            this[resource] = (this[resource] || 0) + added_stack[resource];
+        }
+    }
+
+    remove(removed_stack) {
+        if (!this.isContaining(removed_stack)) {
+            throw new ResourceException();
+        }
+        for (const resource in removed_stack) {
+            this[resource] -= removed_stack[resource];
+        }
+    }
+
     findResourcesDiff(needed_stack) {
-        return {enough: 'enough'}
+        return _.mapKeys(needed_stack, function (resource, qu) {
+
+        });
+        const diff = {};
+        for (const resource in needed_stack) {
+            if (!this.hasOwnProperty(resource)) {
+                this[resource] = 0;
+            }
+            diff[resource] = needed_stack[resource];
+        }
+        return new ResourceStack(diff);
     }
 }
 
--- src/core/models/__tests__/ResourceStack.js
+++ src/core/models/__tests__/ResourceStack.js
@@ -8,8 +8,17 @@ class TestResourceStack extends ResourceStack {
 }
 
 
-it('raises an error on using unknown resource name', () => {
-    expect(
+test('adding another resource stack', () => {
+    const stack = new TestResourceStack({red: 2});
+    const new_stack = new TestResourceStack({blue: 1, red: 1});
+    expect(stack.add(new_stack)).objectContaining({
+        blue: 1,
+        red: 3,
+    })
+});
+
+test('raises an error on using undefined resource name', () => {
+    expect(() => {
         new TestResourceStack({'blue': 1, 'green': 2, 'white': 3})
-    ).toThrowError(ResourceException);
+    }).toThrowError(ResourceException);
 });
