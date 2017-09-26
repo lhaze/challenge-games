69e8237c7c90f640ab71544b44a0884abd7819e6
Lukasz Haze <lhaze@lhaze.name>
2017-09-27 01:24:58 +0200

    Refs #10. React showcase foundations
    * Unnecessary HTML & CSS removed
    * Sample components built [WIP]
    * Dependency on react-bootstrap

--- src/react_showcase/ReactShowcase.js
+++ src/react_showcase/ReactShowcase.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
+import { Button, Col, Grid, Row } from 'react-bootstrap';
 import logo from './logo.svg';
 import './ReactShowcase.css';
 
 
-class ReactShowcase extends Component {
+export default class ReactShowcase extends Component {
     render() {
         return (
             <div className="ReactShowcase">
               <div className="ReactShowcase-header">
                 <img src={logo} className="ReactShowcase-logo" alt="logo" />
-                <h2>challenge-games React showcase</h2>
               </div>
-              <p className="ReactShowcase-intro">
-              </p>
+              <Screen />
             </div>
         );
     }
 }
 
-export default ReactShowcase;
+class Screen extends Component {
+    render() {
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <Mapboard md={8} xs={8} />
+                    <ResourceTray mdOffset={1} xsOffset={1} md={3} xs={3} />
+                </Row>
+            </Grid>
+        );
+    }
+}
+
+class Mapboard extends Component {
+    render() {
+        return (
+            <Col {... this.props }>
+                <h5>Mapboard</h5>
+                <Button bsStyle="warning">Fooo</Button>
+            </Col>
+        );
+    }
+}
+
+class ResourceTray extends Component {
+    render() {
+        return (
+            <Col {... this.props }>
+                <h5>Resource Tray</h5>
+            </Col>
+        );
+    }
+}
